e00c5ed78d22f83e636da7a7ae23286e849ecb69
Watchful1 <watchful@watchful.gr>
2026-02-16 16:35:14 -0800

    Implement button, remove nuke code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { Devvit } from "@devvit/public-api";
 
 /** Reddit username (without u/) to receive PMs when mods use "Report to account" on posts. */
-const TARGET_ACCOUNT = "YourBotAccount";
+const TARGET_ACCOUNT = "CustomModBot";
 
 Devvit.configure({
   redditAPI: true,
@@ -22,7 +22,6 @@ Devvit.addMenuItem({
       const post = await context.reddit.getPostById(context.postId);
       const user = await context.reddit.getCurrentUser();
       const postUrl = `https://www.reddit.com${post.permalink}`;
-
       await context.reddit.sendPrivateMessage({
         to: TARGET_ACCOUNT,
         subject: `Post report from r/${context.subredditName}`,
